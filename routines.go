9ef94c1d09f26a3c4c8928d0b34b5657ef19320c
guilledipa <guilledipasquale@gmail.com>
2019-05-29 20:50:39 +1000

    Initial commit

---
+++ routines.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+const (
+	winDir   = "d:/Test"
+	routines = 10
+)
+
+var wg sync.WaitGroup
+
+func findFiles(dirPath string, c chan os.FileInfo) {
+
+	files, err := ioutil.ReadDir(winDir)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, file := range files {
+		c <- file
+	}
+	close(c)
+}
+
+func worker(c chan os.FileInfo) {
+
+}
+
+func handler(c chan os.FileInfo, i int) {
+	for f := range c {
+		fmt.Println(i, f.Name())
+	}
+	defer wg.Done()
+}
+
+func main() {
+
+	c := make(chan os.FileInfo)
+	go findFiles(winDir, c)
+
+	for i := 0; i < routines; i++ {
+		wg.Add(1)
+		go handler(c, i)
+	}
+
+	wg.Wait()
+	fmt.Println("main finished")
+}
